29c7127b20c0e233ac903edfcc0dd60ec7641865
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-05-30 20:07:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,8 +22,6 @@ class App extends Component {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
 
-    console.log(parsedContacts);
-
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
     }
